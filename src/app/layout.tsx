20461d9abc9b2eed85ecdb3ac1c42c6903d4155c
HANIF <hanif19001@gmail.com>
2026-04-04 00:12:53 +0700

    เพิ่มโลโก้ฟิตและเปลี่ยนชื่อ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "ระบบฐานข้อมูลบุคลากรและทำเนียบค่าย",
+  title: "fityatulhaq",
   description: "Modern Personnel and Camp Management System",
 };
 
